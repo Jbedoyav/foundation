a4cc14d624368e9049e9adea7178833f548f3b23
jbedoyav <jbedoyav@grupo-exito.com>
2024-04-15 20:53:44 -0500

    📦 NEW:

--- lib/design_system/tokens/theme.dart
+++ lib/design_system/tokens/theme.dart
@@ -10,7 +10,7 @@ class Themes {
       displayLarge: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
       displayMedium: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
       displaySmall: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-      headlineLarge: TextStyle(fontSize: 21.0, color: BaseColors.brandPrimary),
+      headlineLarge: TextStyle(fontSize: 21.0, color: BaseColors.bgWhite),
       bodyLarge: TextStyle(fontSize: 14.0),
       bodyMedium: TextStyle(fontSize: 13.0),
       bodySmall: TextStyle(fontSize: 10.0),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:foundations/design_system/tokens/colors.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,7 +9,56 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xffffe800),
+        toolbarHeight: 97,
+        backgroundColor: BaseColors.brandPrimaryMat,
+        title: Padding(
+          padding: const EdgeInsets.fromLTRB(16, 45, 16, 12),
+          child: Row(
+            children: [
+              Container(
+                width: 40,
+                height: 40,
+                margin: const EdgeInsets.only(right: 12),
+                decoration: const BoxDecoration(
+                  color: BaseColors.bgWhite,
+                  image: DecorationImage(
+                    image: AssetImage('assets/image/user.png'),
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  'Store Name',
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                decoration: const BoxDecoration(
+                  color: BaseColors.brandPrimary,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                ),
+              ),
+              Container(
+                width: 40,
+                height: 40,
+                margin: const EdgeInsets.only(left: 5),
+                decoration: const BoxDecoration(
+                  color: BaseColors.brandPrimary,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: const Center(
         child: Text('Hola'),
